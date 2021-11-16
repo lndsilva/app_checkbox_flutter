9920d578c294bac4cc37fba1a8fab6e2dc1bbaa0
lndsilva <lndsilva@hotmail.com>
2021-11-16 11:39:21 -0300

    criando checkbox simples

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,62 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
+/// This is the main application widget.
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  static const String _title = 'Flutter Code Sample';
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Checkboxes Demo',
-      debugShowCheckedModeBanner: false,
-      home: CheckboxesDemo(),
+      title: _title,
+      home: Scaffold(
+        appBar: AppBar(title: const Text(_title)),
+        body: const Center(
+          child: MyStatefulWidget(),
+        ),
+      ),
     );
   }
 }
 
-class CheckboxesDemo extends StatefulWidget {
+/// This is the stateful widget that the main application instantiates.
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
+
   @override
-  _CheckboxesDemoState createState() => _CheckboxesDemoState();
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
 }
 
-class _CheckboxesDemoState extends State<CheckboxesDemo> {
-  List<bool> checked = [true, false, false, true];
+/// This is the private State class that goes with MyStatefulWidget.
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  bool isChecked = false;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Checkboxes Demo'),
-      ),
-      body: Container(
-        child: Column(
-          children: [
-            for (var i = 0; i < 5; i += 1)
-              Row(
-                children: [
-                  Checkbox(
-                      value: value, onChanged: i == 4 ? null : (bool value) {})
-                ],
-              ),
-          ],
-        ),
-      ),
+    Color getColor(Set<MaterialState> states) {
+      const Set<MaterialState> interactiveStates = <MaterialState>{
+        MaterialState.pressed,
+        MaterialState.hovered,
+        MaterialState.focused,
+      };
+      if (states.any(interactiveStates.contains)) {
+        return Colors.blue;
+      }
+      return Colors.red;
+    }
+
+    return Checkbox(
+      checkColor: Colors.white,
+      fillColor: MaterialStateProperty.resolveWith(getColor),
+      value: isChecked,
+      onChanged: (bool? value) {
+        setState(() {
+          isChecked = value!;
+        });
+      },
     );
   }
 }
